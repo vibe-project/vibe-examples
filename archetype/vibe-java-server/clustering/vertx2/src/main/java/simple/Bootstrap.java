9a5ab2f963524fb023027117a95f63c9ca081bc2
Donghwan Kim <flowersinthesand@gmail.com>
2014-07-07 19:30:15 +0900

    Add archetype example of vibe-java-server clustered by Vert.x2

---
+++ archetype/vibe-java-server/clustering/vertx2/src/main/java/simple/Bootstrap.java
@@ -0,0 +1,86 @@
+package simple;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Map;
+
+import org.atmosphere.vibe.Action;
+import org.atmosphere.vibe.runtime.ClusteredServer;
+import org.atmosphere.vibe.runtime.Socket;
+import org.atmosphere.vibe.vertx.VertxBridge;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.platform.Verticle;
+
+public class Bootstrap extends Verticle {
+    @Override
+    public void start() {
+        // You need to set cluster configuration from vertx-maven-plugin to true to enable distributed event bus
+        final EventBus eventBus = vertx.eventBus();
+        final ClusteredServer server = new ClusteredServer();
+        eventBus.registerHandler("vibe", new Handler<Message<byte[]>>() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public void handle(Message<byte[]> message) {
+                // Message's body's type bytes generated from Map object
+                // retrieve original object
+                ByteArrayInputStream bais = new ByteArrayInputStream(message.body());
+                Map<String, Object> body = null;
+                try (ObjectInputStream in = new ObjectInputStream(bais)) {
+                    body = (Map<String, Object>) in.readObject();
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("receiving a message: " + body);
+                server.messageAction().on(body);
+            }
+        });
+        server.publishAction(new Action<Map<String,Object>>() {
+            @Override
+            public void on(Map<String, Object> message) {
+                System.out.println("publishing a message: " + message);
+                // EventBus doesn't allow to publish object though it is Serializable
+                // so convert it to byte array and publish it instead
+                ByteArrayOutputStream baos = new ByteArrayOutputStream(512);
+                try (ObjectOutputStream out = new ObjectOutputStream(baos)) {
+                    out.writeObject(message);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                eventBus.publish("vibe", baos.toByteArray());
+            }
+        });
+        
+        server.socketAction(new Action<Socket>() {
+            @Override
+            public void on(final Socket socket) {
+                System.out.println("on socket: " + socket.uri());
+                socket.on("echo", new Action<Object>() {
+                    @Override
+                    public void on(Object data) {
+                        System.out.println("on echo event: " + data);
+                        socket.send("echo", data);
+                    }
+                });
+                socket.on("chat", new Action<Object>() {
+                    @Override
+                    public void on(Object data) {
+                        System.out.println("on chat event: " + data);
+                        server.all().send("chat", data);
+                    }
+                });
+            }
+        });
+
+        HttpServer httpServer = vertx.createHttpServer();
+        new VertxBridge(httpServer, "/vibe").httpAction(server.httpAction()).websocketAction(server.websocketAction());
+        httpServer.listen(Integer.parseInt(System.getProperty("vertx.port")));
+    }
+}
