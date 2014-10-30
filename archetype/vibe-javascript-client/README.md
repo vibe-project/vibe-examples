cd0ab2368f20d99ae4bce6e659a696ca483c1168
Donghwan Kim <flowersinthesand@gmail.com>
2014-10-30 20:11:56 +0900

    Update JavaScript client code

--- archetype/vibe-javascript-client/README.md
+++ archetype/vibe-javascript-client/README.md
@@ -2,5 +2,5 @@ To run on
 
 * Node.js: type `npm install vibe-client` and `node main`.
 * Browser
-    * via cross origin: execute the prepared code from [JS Bin](http://jsbin.com/soqugi/1/watch?js,console).
+    * via cross origin: execute the prepared code from [JS Bin](http://jsbin.com/ditewo/1/watch?js,console).
     * via same origin: copy `index.html` and `main.js` to your server's assets directory, run the server and open `http://localhost:8080/index.html` in browser.
--- archetype/vibe-javascript-client/main.js
+++ archetype/vibe-javascript-client/main.js
@@ -4,16 +4,22 @@ try {
 } catch (e) {}
 
 var socket = vibe.open("http://localhost:8080/vibe", {reconnect: false});
-socket.on("open", function() {
+socket.on("connecting", function() {
+    console.log("on connecting event");
+})
+.on("open", function() {
     console.log("on open event");
     socket.send("echo", "An echo message").send("chat", "A chat message");
 })
-.on("close", function(reason) {
-    console.log("on close event: " + reason);
+.on("error", function(error) {
+    console.log("on error event", error);
+})
+.on("close", function() {
+    console.log("on close event");
 })
 .on("chat", function(data) {
-    console.log("on chat event: " + data);
+    console.log("on chat event", data);
 })
 .on("echo", function(data) {
-    console.log("on echo event: " + data);
+    console.log("on echo event", data);
 });
